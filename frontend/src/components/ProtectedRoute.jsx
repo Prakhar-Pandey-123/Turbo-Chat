a4372c04d752cebd8687decc7217c832e995fdc4
prakhar pandey <prakhar9704@gmail.com>
2026-02-22 15:19:57 +0530

    fixed auth

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,31 +1,34 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import {Navigate } from "react-router-dom"
 
 const ProtectedRoute=({children})=>{
      const baseurl = import.meta.env.VITE_BASE_URL
-    let token=localStorage.getItem("token")
+     const [isAuth,setIsAuth]=useState(null);
+
+    // let token=localStorage.getItem("token")
     useEffect(()=>{
         fn()
     },[])
     const fn=async()=>{
         try{
         const data=await axios.post(baseurl+"/check",{
-    
         },{
             withCredentials:true
         })
-        return children
+        setIsAuth(true)
     }
     catch(e){
-       token="";
+        setIsAuth(false);
+    //    token="";
+    //     if(token===""){
+    //     console.log("directed from protected route")
+    //     return <Navigate to ="/login" ></Navigate>
+    // }
     }
     }
-    
-    if(token===""){
-        console.log("directed from protected route")
-        return <Navigate to ="/login" ></Navigate>
-    }
+    if(isAuth===null) return <div>Loading...</div>
+    if(!isAuth) return <Navigate to="/login"></Navigate>
     return children;
 }
 export default ProtectedRoute
